353e18b30566d3c95323c16d1a60440716ff5302
ryanfawcett <tangch3ng@gmail.com>
2024-03-16 21:06:00 +0800

    feat: 为账户随机生成8位数的账号

--- src/cn/atm/ATM.java
+++ src/cn/atm/ATM.java
@@ -2,6 +2,7 @@ package cn.atm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -48,7 +49,7 @@ public class ATM {
     /**
      * 用户开户
      */
-    public void createAccount() {
+    private void createAccount() {
         // 1. 创建一个账户对象，用于封装用户的开户信息
         Account account = new Account();
 
@@ -86,10 +87,39 @@ public class ATM {
 
         // 注意：我们需要为这个账户生成一个卡号
         // 由系统自动生成，8位数字表示，并且唯一，即不能与账户集合中的其他卡号重复
-
+        while (true) {
+            String cardId = generateCardId(); // 随机生成一个8位数的账号
+            // 在账号集合中查询生成的账号是否存在
+            // 如果存在则重新生成，如果不存在则将生成的账号赋值给当前账户并且退出循环
+            boolean exist = accounts.parallelStream()
+                    .anyMatch(a -> cardId.equals(a.getCardId()));
+            if (!exist) {
+                account.setCardId(cardId);
+                break;
+            }
+        }
 
         // 3. 把这个账户对象添加到账户集合中
         accounts.add(account);
-        System.out.println("恭喜您-" + account.getUsername() + "，开户成功！您的卡号是：");
+        System.out.println("恭喜" + account.getUsername() + "开户成功！您的卡号是：" + account.getCardId());
+    }
+
+    /**
+     * 生成一个8位数字的随机数
+     *
+     * @return cardId
+     */
+    private String generateCardId() {
+        // 1. 定义一个用于记录cardId的字符串
+        StringBuilder cardId = new StringBuilder();
+
+        // 2. 使用循环随机生成数字，并将生成的数字进行拼接
+        // 2.1 定义Random对象，用于随机生成数字
+        Random random = new Random();
+        for (int i = 0; i < 8; i++) {
+            int unit = random.nextInt(10); // 表示生成 0 - 9 之间的数字
+            cardId.append(unit);
+        }
+        return cardId.toString();
     }
 }
